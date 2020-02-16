1abe8886bc06f8c7473170612712c5cc85ae7373
Wachirawit Onchet <ingwachi@gmail.com>
2020-02-16 13:31:31 +0700

    Content 1

--- resources/views/dessert.blade.php
+++ resources/views/dessert.blade.php
@@ -1,8 +1,175 @@
 @extends('layouts.master')
 
 @section('content')
-    <div style="font-family: 'Kanit', sans-serif;">
+    <div style="font-family: 'Kanit', sans-serif; margin-top: 3%">
+        <div style="font-size: 40px; margin-left: 5%;">3 อันดับคาเฟ่ยอดฮิตย่านเกษตร</div>
+        <hr style="width: 90%; border: 1px solid black">
+        <div class="row">
+            <div class="col-9">
+                <div style="font-size: 30px; margin-left: 7%">1. Too Fast To Sleep @KU</div>
+                <div class="row">
+                    <div class="col">
+                        <img src="/img/content1/1/tofasttosleep.jpg" alt="" style="width: 90%; margin-top: 2%; margin-left: 15%">
+                    </div>
+                    <div class="col" style="margin-left: 2%">
+                        <div style="margin-top: 7%">ตำแหน่งที่ตั้ง</div>
+                        <div style="color: #4e555b">50 อาคาร KU Ville Tweesackdi Sesaweech Rd, Lat Yao, Chatuchak, Bangkok 10900</div>
+                        <div style="margin-left: 8%">มาแล้วกับ Too Fast To Sleep@KU เปิดให้บริการบริเวณ KU VILIE</div>
+                        <div>โซนอาคารหอพักนิสิตและบุคลากรของมหาวิทยาลัยเกษตรศาสตร์ บางเขน แถมยังคงคอนเซปต์เปิดให้บริการ 24 ชั่วโมงเหมือนเดิมทุกวันมีบริการห้องอ่านหนังสือและ WIFI ห้องอาหาร เครื่องดื่ม และเบเกอรี่บริการ ชิลล์เลยงานนี้ล่าสุดยังมีบริการเปิดโซนใหม่อย่าง <b>KU & Friends</b> สำหรับรองรับนิสิตและบุคลากรชาวเกษตรอีกด้วย</div>
+                    </div>
+                </div>
+                <div style="margin-left: 7%; margin-top: 5%">
+                    <div style="font-size: 20px;">รูปภาพเพิ่มเติม</div>
+                    <hr style="border: 2px solid black; width: 10%; margin-left: -0%; border-radius: 10px">
+                    <div class="row">
+                        <div class="col">
+                            <img src="/img/content1/1/1.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/1/2.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">พื้นที่อ่านหนังสือสุดชิลล์</div>
+                    <div class="row" style="margin-top: 2%">
+                        <div class="col">
+                            <img src="/img/content1/1/3.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/1/4.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">เครื่องดื่มของหวานพร้อม</div>
+                    <div class="row" style="margin-top: 2%">
+                        <div class="col">
+                            <img src="/img/content1/1/5.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/1/6.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">โซน KU & Friends</div>
+                </div>
 
+                <div style="font-size: 30px; margin-left: 7%; margin-top: 8%">2. R.E.A.D Cafe </div>
+                <div class="row">
+                    <div class="col">
+                        <img src="/img/content1/2/1.jpg" alt="" style="width: 92%; margin-top: 2%; margin-left: 15%">
+                    </div>
+                    <div class="col" style="margin-left: 2%">
+                        <div style="margin-top: 5%">ตำแหน่งที่ตั้ง</div>
+                        <div style="color: #4e555b">
+                            21/50, ซอยงามวงศ์วาน 54, ลาดยาว, กรุงเทพมหานคร, 10900, ประเทศไทย กรุงเทพมหานคร</div>
+                        <div style="margin-left: 8%">พบกับ R.E.A.D Cafe บริเวณซอยงามวงศ์วาน 54 ที่มีถึง 3 ชั้น</div>
+                        <div>ร้าน R.E.A.D Cafe ร้านกาแฟที่มีขายทั้งกาแฟและเครื่องดื่ม รวมไปถึงอาหารต่างๆ ทั้งอาหารทานเล่นและเมนูอาหารหลัก ที่สำคัญคือเปิด 24 ชั่วโมง น้องๆ คนไหนจะอ่านหนังสือ ติวเข้ม ก่อนสอบพรุ่งนี้เช้า ก็สามารถมานั่งอ่านหนังสือได้ แถมมีกาแฟและเครื่องดื่มให้บริการอีกด้วย Free WIFI พร้อมซะขนาดนี้ นั่งได้ยาวๆ</div>
+                    </div>
+                </div>
+                <div style="margin-left: 7%; margin-top: 5%">
+                    <div style="font-size: 20px;">รูปภาพเพิ่มเติม</div>
+                    <hr style="border: 2px solid black; width: 10%; margin-left: -0%; border-radius: 10px">
+                    <div class="row">
+                        <div class="col">
+                            <img src="/img/content1/2/2.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/2/3.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">ครบเครื่องทั้งของคาวของหวาน</div>
+                    <div class="row" style="margin-top: 2%">
+                        <div class="col">
+                            <img src="/img/content1/2/6.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/2/7.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">เครื่องดื่มรสชาติดีมีหลากหลายให้เลือกสรร</div>
+                    <div class="row" style="margin-top: 2%">
+                        <div class="col">
+                            <img src="/img/content1/2/4.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/2/5.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">มีหลากหลยมุมให้เลือกนั่ง</div>
+                </div>
+
+                <div style="font-size: 30px; margin-left: 7%; margin-top: 8%">3. FitB - Fuel in the Blank </div>
+                <div class="row">
+                    <div class="col">
+                        <img src="/img/content1/3/1.jpg" alt="" style="width: 92%; margin-top: 2%; margin-left: 15%">
+                    </div>
+                    <div class="col" style="margin-left: 2%">
+                        <div>ตำแหน่งที่ตั้ง</div>
+                        <div style="color: #4e555b">
+                            85 ถนน งามวงศ์วาน แขวง ลาดยาว จตุจักร กรุงเทพมหานคร 10900</div>
+                        <div style="margin-left: 8%"> เบื่อไหมเวลาไปคาเฟ่แล้วแสงน้อยทำให้รูปที่ได้ออกมาดูดรอปลงไม่ได้</div>
+                        <div>ดั่งใจแต่วันนี้ไม่ต้องกังวลไปแค่มาร้าน FitB Fuel in the Blank ก็ทำให้ความสุขของการถ่ายรูปกลับมาอีกครั้ง ด้วยร้านที่ตกแต่งค่อนข้างปลอดโปร่งและมีต้นไม้ผสมมานิดๆ ให้อารมณ์แบบธรรมชาติมาก แถมร้านยังตกแต่งสไตล์วินเทจด้วยการเอาไม้พาเลทมาทำเป็นที่นั่ง ในส่วนเรื่องอาหารทานเล่นที่อยากแนะนำต้องนี่เลย FUEL FRIES (Paprika, Sour Cream, Cheese) ราคา 75 บาท และ Sweet Fudgy Brownies ราคา 150 บาท งานนี้นอกจากจะได้ทานขนมอร่อยๆ ยังได้ถ่ายรูปกับมุมต่างๆ ด้วยนะ คุ้มสุดๆ</div>
+                    </div>
+                </div>
+                <div style="margin-left: 7%; margin-top: 5%">
+                    <div style="font-size: 20px;">รูปภาพเพิ่มเติม</div>
+                    <hr style="border: 2px solid black; width: 10%; margin-left: -0%; border-radius: 10px">
+                    <div class="row">
+                        <div class="col">
+                            <img src="/img/content1/3/2.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/3/3.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">มุมถ่ายรูปแสงสวยๆกับธรรมชาติสีเขียว</div>
+                    <div class="row" style="margin-top: 2%">
+                        <div class="col">
+                            <img src="/img/content1/3/4.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/3/5.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%">งานคาวหวานก็มาถ่ายรูปลง IG ได้เลย</div>
+                    <div class="row" style="margin-top: 2%">
+                        <div class="col">
+                            <img src="/img/content1/3/6.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                        <div class="col">
+                            <img src="/img/content1/3/7.jpg" alt="" style="width: 450px; height: 300px">
+                        </div>
+                    </div>
+                    <div style="text-align: center; margin-top: 2%; margin-bottom: 5%">งานเล็กงานใหญ่รองรับหมด</div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div>
+                    <div style="margin-left: 10%; font-size: 20px">บทความน่าสนใจอื่นๆ</div>
+                    <div class="card" style="width: 16rem; height: 27rem">
+                        <img class="card-img-top" src="/img/homepage/content2.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">5 ร้านอาหารน่าลองย่านเกษตร <span class="badge badge-danger">New</span></h5>
+                            <p class="card-text">5 อันดับร้านอาหารเด็ดยอดฮิตของเด็กเกษตรไม่มาถือว่ามาไม่ถึงเกษตร...</p>
+                            <a href="#" class="btn btn-info" style="margin-left: 25%">อ่านบทความ</a>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 16rem; height: 27rem; margin-top: 3%">
+                        <img class="card-img-top" src="/img/homepage/content3.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">เอ่งฉ้วนสุกี้ร้านใหม่งามวงศ์วาน <span class="badge badge-danger">New</span></h5>
+                            <p class="card-text">ร้านของเราเป็นอาหารจีนสไตล์ฮกเกี้ยนภูเก็ต สูตรอาหารที่เราทำเป็นมรดกตกทอดมาจากอากง และอาม่า...</p>
+                            <a href="#" class="btn btn-info" style="margin-left: 25%">อ่านบทความ</a>
+                        </div>
+                    </div>
+                    <div class="card" style="width: 16rem; height: 27rem; margin-top: 3%">
+                        <img class="card-img-top" src="/img/homepage/content4.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">มาถึงเกษตรแล้วกับ Mihimihi</h5>
+                            <p class="card-text">ขนมสไตล์ฝรั่งเศส แป้งบางมีความกรอบ ชิ้นใหญ่ มีหลายไส้ให้เลือก...</p>
+                            <a href="#" class="btn btn-info" style="margin-left: 25%">อ่านบทความ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
 @endsection
